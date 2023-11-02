71f7f5a4e1a70f81a28a0e9977b400267e8b8b3d
HoYoung1 <kimo722504@gmail.com>
2023-11-02 22:24:55 +0800

    Complete msg format

---
+++ main.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+
+"""
+<사전 출석체크>
+10월 31일 화요일, 저녁 운동 출석 체크하겠습니다. (19:00~22:00)
+"""
+
+
+def main():
+    text_dic = {
+        0: ["월요일", "오전", "09:30～12:00"],
+        1: ["화요일", "저녁", "19:00～22:00"],
+        2: ["수요일", "오전", "09:30～12:00"],
+        3: ["목요일", "저녁", "19:00～22:00"],
+        4: ["금요일", "오전", "09:30～12:00"],
+        5: ["토요일", "주말", "14:00～17:00"],
+        6: ["일요일", "주말", "14:00～17:00"],
+    }
+
+    tomorrow: datetime = datetime.now() + timedelta(1)
+
+    text_monthday = tomorrow.strftime('%m월 %d일')
+    text_weekday = text_dic[tomorrow.weekday()][0]
+    text_timetype = text_dic[tomorrow.weekday()][1]
+    text_timefromto = text_dic[tomorrow.weekday()][2]
+
+    msg = f"""<사전 출석체크>
+{text_monthday} {text_weekday}, {text_timetype} 운동 출석 체크하겠습니다. ({text_timefromto})
+    """
+    print(msg)
+
+
+if __name__ == '__main__':
+    main()
